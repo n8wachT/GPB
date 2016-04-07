1450e237b9f16d5ce0dc482becb5579831d59da3
Sanguchi <22matias1994@gmail.com>
2016-04-07 11:13:39 -0300

    updated who plugin

--- py2/plugins/Who.py
+++ py2/plugins/Who.py
@@ -4,20 +4,37 @@ class Who(Plugin):
 
     def on_message(self, message):
         super(Who, self).on_message(message)
-        na, us, ui, gi = (
-        message.from_user.first_name,
-        message.from_user.username,
-        str(message.from_user.id),
-        str(message.chat.id))
-        pe = u'\U0001f464'
-        ge = u'\U0001f465'
-        if(message.chat.username):
-            gn = '@' + message.chat.username
-        else:
-            gn = message.chat.title or message.chat.first_name
-        text = 'Info about ' + na + ':\n' + pe + ' @'+us+'['+ui+']\n'+pe+ ' ' + gn + '['+gi+']'
-
-        self.bot.send_message(self.cid, text)
+        self.bot.send_message(self.cid, user_info(message))
 
     def get_help(self):
         return "who\nPlugin that returns user data."
+    
+def user_info(message):
+    pemji = u'\U0001f464'
+    gemji = u'\U0001f465'
+    if message.reply_to_message:
+        fname = message.reply_to_message.from_user.first_name
+        usrid = str(message.reply_to_message.from_user.id)
+        uname = message.reply_to_message.from_user.username or 'He doesn\'t have username!'
+        grpid = str(message.reply_to_message.chat.id)
+        gname = message.reply_to_message.chat.title or 'No chat title!'
+        gunme = message.chat.username or gname
+        msgfm = u'Info about {}:\n{} @{}[{}]\n{} {}[{}]'.format(fname, pemji, uname, usrid, gemji, gname, grpid)
+        if message.reply_to_message.chat.type == 'group':
+            return msgfm
+        if message.reply_to_message.chat.type == 'supergroup':
+            return msgfm
+    fname = message.from_user.first_name
+    usrid = str(message.from_user.id)
+    uname = message.from_user.username or 'You don\'t have username!'
+    grpid = str(message.chat.id)
+    gname = message.chat.title or 'No chat title!'
+    gunme = message.chat.username or gname
+    msgfm = u'Info about {}:\n{} @{}[{}]\n{} {}[{}]'.format(fname, pemji, uname, usrid, gemji, gname, grpid)
+    if message.chat.type == 'private':
+        return u'Info about {}:\n{} @{}[{}]'.format(fname, pemji, uname, usrid)
+    if message.chat.type == 'group':
+        return msgfm
+    if message.chat.type == 'supergroup':
+        return msgfm
+    return 'No Info'
