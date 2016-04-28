9aa14a368b5bfc7c5c7e24c2575d5136d88d2d43
Sanguchi <22matias1994@gmail.com>
2016-04-27 22:45:40 -0300

    Removed trash code in Settings.py
    Just removed old comments and added new comments to help.

--- py2/Settings.py
+++ py2/Settings.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 import telebot
 from Utils import build_plugins, init_groups
-#from SDBApi import SDBApi
-#server = SDBApi()
+#Put your id here to use admin-only plugins.
 admins = [59802458]
-disabled_files = ['Store.py']
+#List of plugins that will not be loaded. Ex. ['Echo.py', 'Who.py']
+disabled_files = []
+#Plugins that will be disabled at runtime if they throws exceptions.
+#Don't edit.
 disabled_plugins = []
+#Set to True if you want to disable permanently a plugin that throws
+#exceptions.
 strict_errors = False
+#Starting character for commands.
 command_char = '/'
 #PASTE YOUR TOKEN HERE 
 token = ''
 if(token == ''):
     token = raw_input('No token detected, please paste or type here your token:\n> ')
 print('token = ['+token+']')
+##INITIALIZATION SECTION.
 groups = init_groups()
 bot = telebot.TeleBot(token)
 me = bot.get_me()
@@ -24,4 +30,6 @@ print('Bot nickname: [' + nickname + ']')
 print('Command char: [' + command_char + ']')
 
 plugins = build_plugins(bot, 'plugins', disabled_files)
+#Chat id's that will be ignored at runtime, edit to ignore permanently
+#Across executions. Ex. [1234567, -9876543]
 ignored_chats = []
